d139b0c9f82caf84caacd6e077b78dd854db3296
Matthew <mattyHoseini@gmail.com>
2025-08-07 05:02:32 -0700

    Add directional controls for snake using arrow keys

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 // Define the size of each snake segment
 let scale = 10;
+
 // Define the Snake class
 function Snake() {
-   // Initial position of the snake 
+  // Initial position of the snake
   this.x = 0;
   this.y = 10;
   // Initial movement direction (moving right)
   this.xSpeed = scale;
   this.ySpeed = 0;
-// Draw the snake on the canvas
+  // Draw the snake on the canvas
   this.snakeDraw = function () {
     ctx.fillStyle = "#fff";
     ctx.fillRect(this.x, this.y, scale, scale);
@@ -23,13 +24,46 @@ function Snake() {
     this.x += this.xSpeed;
     this.y += this.ySpeed;
   };
+
+  //Function to control the direction of snake movement
+  this.updateDirection = function (userDirection) {
+    switch (userDirection) {
+      case "Up": {
+        this.xSpeed = 0
+        this.ySpeed = -scale
+        break;
+      }
+      case "Down": {
+        this.xSpeed = 0
+        this.ySpeed = +scale
+        break;
+      }
+      case "Left": {
+        this.xSpeed = -scale
+        this.ySpeed = 0
+        break;
+      }
+      case "Right": {
+        this.xSpeed = +scale
+        this.ySpeed = 0
+        break;
+      }
+    }
+  };
 }
+
 // Start the game once the window is fully loaded
 window.addEventListener("load", () => {
-  let snake = new Snake();// Create a new snake instance
+  let snake = new Snake(); // Create a new snake instance
   setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     snake.snakeDraw();
     snake.updateLocation();
   }, 100);
+  // Listen for keyboard input to control the snake
+  window.addEventListener("keydown", (event) => {
+    let userDirection = event.key.replace("Arrow", "")
+    snake.updateDirection(userDirection)
+    console.log(userDirection);
+  });
 });
